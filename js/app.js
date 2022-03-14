d7b0cb6a2585d6baa03551159caa108cee7200d5
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-03-14 13:19:43 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+ let navLink = document.getElementById('nav')
+//  console.log('kk')
+ function menuBar(){
+     navLink.style.right='0px'
+ }
+ function closeMenu(){
+    //  console.log('menu');
+    navLink.style.right='-200px'
+ }
